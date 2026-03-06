a992f6a11dd8d8d894b0d71ece721113546a182f
Huer <97923571+HuerRome@users.noreply.github.com>
2026-03-06 10:25:48 -0300

    Adding functionality to the product page

---
+++ Javascript/producto/script.js
@@ -0,0 +1,61 @@
+"use strict";
+
+// Punto de entrada
+document.addEventListener("DOMContentLoaded", () => {
+  // JS listo para usar
+});
+
+const breadcrumb = [
+  {name:"Frávega",url:"/"},
+  {name:"Pequeños Electrodomésticos",url:"/electrodomesticos"},
+  {name:"Hogar",url:"/hogar"},
+  {name:"Limpiadoras a vapor",url:null}
+];
+
+/*----------------------- Galeria de imagenes ------------------------------*/
+const mainImage = document.getElementById("mainImage");
+const thumbnails = document.querySelectorAll(".thumb");
+const zoomArea = document.querySelector(".zoom-area");
+
+
+// CAMBIO DE IMAGEN
+
+thumbnails.forEach(thumb => {
+
+thumb.addEventListener("click", () => {
+
+thumbnails.forEach(t => t.classList.remove("active"));
+
+thumb.classList.add("active");
+
+mainImage.src = thumb.src;
+
+});
+
+});
+
+
+
+// ZOOM DINAMICO
+
+zoomArea.addEventListener("mousemove", (e)=>{
+
+const rect = zoomArea.getBoundingClientRect();
+
+const x = (e.clientX - rect.left) / rect.width * 100;
+const y = (e.clientY - rect.top) / rect.height * 100;
+
+mainImage.style.transformOrigin = `${x}% ${y}%`;
+
+zoomArea.classList.add("zoomed");
+
+});
+
+
+zoomArea.addEventListener("mouseleave", ()=>{
+
+zoomArea.classList.remove("zoomed");
+
+mainImage.style.transformOrigin = "center";
+
+});
